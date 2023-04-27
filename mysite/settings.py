58f41968f2e839f13dc636ed6352ddc28b3d9ef4
adriabalmes <aberruez@jaumebalmes.net>
2023-04-27 20:54:36 +0200

    proyecto base env

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'NAME': "railway",
+        'USER': "postgres",
+        'PASSWORD': "OzlNeW9WxSDpVtIUYGmS",
+        'HOST': "containers-us-west-162.railway.app",
+        'PORT': "6939",
     }
 }
 
